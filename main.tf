89381d5d6a0852ee3fe53564af1f3502a807442c
Benoît Vézina <benoit@vezina.biz>
2023-02-11 10:53:47 -0500

    removed aws

--- main.tf
+++ main.tf
@@ -1,36 +1,7 @@
-provider "aws" {
-  region = var.region
-}
-
 provider "proxmox" {
   pm_tls_insecure = true
   pm_api_url = "https://pve01.durpro.com:8006/api2/json"
   pm_otp = ""
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
 }
 
 resource "proxmox_lxc" "basic" {
--- versions.tf
+++ versions.tf
@@ -1,18 +1,9 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
     proxmox = {
       source  = "telmate/proxmox"
       version = ">= 2.9.5"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
   }
-
   required_version = ">= 0.14.0"
 }
