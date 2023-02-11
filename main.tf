2a505c8e7b98e56d44d8e1345d4933367522c357
Benoît Vézina <benoit@vezina.biz>
2023-02-11 10:36:16 -0500

    new promox provider remove some line

--- main.tf
+++ main.tf
@@ -5,8 +5,6 @@ provider "aws" {
 provider "proxmox" {
   pm_tls_insecure = true
   pm_api_url = "https://pve01.durpro.com:8006/api2/json"
-  pm_password = "password"
-  pm_user = "root@pam"
   pm_otp = ""
 }
 
