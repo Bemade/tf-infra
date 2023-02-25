acb8dfb6361fa484b778dfdac30bd3475e70724b
Bemade <125137163+Bemade@users.noreply.github.com>
2023-02-25 07:33:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,50 @@ resource "proxmox_lxc" "basic" {
     ip     = "dhcp"
   }
 }
+
+resource "proxmox_vm_qemu" "cloudinit-test" {
+    name = "terraform-test-vm"
+    desc = "A test for using terraform and cloudinit"
+
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "pve01"
+
+    # The destination resource pool for the new VM
+    pool = "pool0"
+
+    # The template name to clone this vm from
+    clone = "debian10ci"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 2
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 2048
+    scsihw = "lsi"
+
+    # Setup the disk
+    disk {
+        size = 8
+        type = "virtio"
+        storage = "local"
+    }
+
+    # Setup the network interface and assign a vlan tag: 256
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+        tag = 256
+    }
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.10.20/24,gw=192.168.10.1"
+    sshkeys = <<EOF
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEsNLo8NDKurxAIO4gbLS7Xqttc3Chjr88SY6uYAhiAT benoit@vezina.biz
+    EOF
+}
